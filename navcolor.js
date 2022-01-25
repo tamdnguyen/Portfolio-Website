663de90f1c22dae09cc870b87bfe58159719a596
tamngx02 <tamdnguyen2111@gmail.com>
2022-01-25 22:20:57 +0200

    completed project section

--- navcolor.js
+++ navcolor.js
@@ -1,7 +1,7 @@
 
 $(function() {
     $(window).scroll(function () {
-       if ($(this).scrollTop() > 200) {
+       if ($(this).scrollTop() > 500) {
           $("nav").addClass("change-color")
        } else {
           $("nav").removeClass("change-color")
