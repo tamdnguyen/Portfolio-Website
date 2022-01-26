ac70d426665b2692b8f0fc998553a5edac5d2280
tamngx02 <tamdnguyen2111@gmail.com>
2022-01-27 00:34:06 +0200

    update hamburger navbar animation

--- hamburger.js
+++ hamburger.js
@@ -1,8 +1,14 @@
 const hamburger = document.getElementById("hamburger");
 const navUL = document.getElementById("navbar-page-content");
 const intro = document.getElementById("intro");
+const about = document.getElementById("about");
+const project = document.getElementById("project");
+const nav = document.getElementById("nav")
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
     intro.classList.toggle('show');
+    about.classList.toggle('show');
+    project.classList.toggle('show');
+    nav.classList.toggle('show');
 });
